96c1f94282b9e979aeb98cba45fe8d3d4f58d7d7
abanoj <jaabanof@gmail.com>
2026-02-19 11:10:08 +0100

    remove toTaskList of taskListResponseDto

--- src/main/java/com/abanoj/tasklist/tasklist/mapper/TaskListMapper.java
+++ src/main/java/com/abanoj/tasklist/tasklist/mapper/TaskListMapper.java
@@ -1,6 +1,5 @@
 package com.abanoj.tasklist.tasklist.mapper;
 
-import com.abanoj.tasklist.auth.SecurityUtils;
 import com.abanoj.tasklist.task.entity.Task;
 import com.abanoj.tasklist.task.entity.TaskStatus;
 import com.abanoj.tasklist.task.mapper.TaskMapper;
@@ -8,32 +7,17 @@ import com.abanoj.tasklist.tasklist.dto.TaskListUpdateRequestDto;
 import com.abanoj.tasklist.tasklist.entity.TaskList;
 import com.abanoj.tasklist.tasklist.dto.TaskListResponseDto;
 import com.abanoj.tasklist.tasklist.dto.TaskListCreateRequestDto;
-import com.abanoj.tasklist.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class TaskListMapper {
 
     private final TaskMapper taskMapper;
-    private final SecurityUtils securityUtils;
-
-    public TaskList toTaskList(TaskListResponseDto taskListResponseDto){
-        User user = securityUtils.getCurrentUser();
-        return new TaskList(
-                taskListResponseDto.id(),
-                taskListResponseDto.title(),
-                Optional.of(taskListResponseDto.tasks().stream().map(taskMapper::toTask).toList()).orElse(null),
-                null,
-                null,
-                user
-        );
-    }
 
     public TaskList toTaskList(TaskListCreateRequestDto taskListRequestDto){
         return TaskList.builder().title(taskListRequestDto.title()).tasks(new ArrayList<>()).build();
