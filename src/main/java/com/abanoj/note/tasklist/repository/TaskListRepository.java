f4a826dafb80fe471f24d625f2883183b1a42d25
abanoj <jaabanof@gmail.com>
2026-02-19 18:38:01 +0100

    Fix lazy loading issues by using JOIN FETCH queries in TaskListRepository

--- src/main/java/com/abanoj/note/tasklist/repository/TaskListRepository.java
+++ src/main/java/com/abanoj/note/tasklist/repository/TaskListRepository.java
@@ -3,6 +3,8 @@ package com.abanoj.note.tasklist.repository;
 import com.abanoj.note.tasklist.entity.TaskList;
 import com.abanoj.note.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,9 @@ import java.util.Optional;
 
 @Repository
 public interface TaskListRepository extends JpaRepository<TaskList, Long> {
-    List<TaskList> findAllByUser(User user);
-    Optional<TaskList> findByIdAndUser(Long id, User user);
+    @Query("SELECT DISTINCT tl FROM TaskList tl LEFT JOIN FETCH tl.tasks WHERE tl.user = :user")
+    List<TaskList> findAllByUser(@Param("user") User user);
+    @Query("SELECT tl FROM TaskList tl LEFT JOIN FETCH tl.tasks WHERE tl.id = :id AND tl.user = :user")
+    Optional<TaskList> findByIdAndUser(@Param("id") Long id, @Param("user") User user);
     boolean existsByIdAndUser(Long id, User user);
 }
