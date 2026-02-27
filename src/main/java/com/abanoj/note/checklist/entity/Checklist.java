91dc2a5f0d8c298a8de0d32dd90406fe97734f21
abanoj <jaabanof@gmail.com>
2026-02-27 12:44:59 +0100

    Add @Builder.Default to items

--- src/main/java/com/abanoj/note/checklist/entity/Checklist.java
+++ src/main/java/com/abanoj/note/checklist/entity/Checklist.java
@@ -24,6 +24,7 @@ public class Checklist {
     @Column(nullable = false)
     private String title;
     @OneToMany(mappedBy = "checklist", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
     private List<Item> items = new ArrayList<>();
     @Column(nullable = false, updatable = false)
     private LocalDateTime created;
